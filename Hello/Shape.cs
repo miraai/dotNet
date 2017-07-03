46f4b67b6e0603db5e212030196c0853d06864d6
miraai <anonymus.nlo@gmail.com>
2017-07-03 14:10:01 -0400

    spaceships

---
+++ Hello/Shape.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hello
+{
+    class Shape
+    {
+        public virtual int Perimeter()
+        {
+            return 0;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public override int Perimeter()
+        {
+            return (Height + Width) * 2;
+        }
+    }
+
+    class Triangle : Shape
+    {
+        public int Side1 { get; set; }
+        public int Side2 { get; set; }
+        public int Side3 { get; set; }
+
+        public override int Perimeter()
+        {
+            return Side1 + Side2 + Side3;
+        }
+    }
+}
---
+++ Hello/Speedometer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hello
+{
+    class Speedometer
+    {
+        private int currentSpeed;
+
+        public int GetCurrentSpeed()
+        {
+            return currentSpeed;
+        }
+
+        public void SetCurrentSpeed(int speed)
+        {
+            if (speed < 0) return;
+            if (speed > 120) return;
+
+            currentSpeed = speed;
+        }
+    }
+
+    class Speedometer2
+    {
+        private int currentSpeed;
+
+        public int CurrentSpeed
+        {
+            get
+            {
+                return currentSpeed;
+            }
+            set
+            {
+                if (value < 0) return;
+                if (value > 120) return;
+
+                currentSpeed = value;
+            }
+        }
+    }
+}
